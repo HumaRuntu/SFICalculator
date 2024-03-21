ae0f21b5fd0ec256adb7cf4ca9194bb7f3d95ecd
IsaacJLambert <eyeslick@gmail.com>
2024-03-21 14:44:32 -0600

    Basic GUI and video can be chosen

--- main.py
+++ main.py
@@ -1,10 +1,64 @@
+import cv2
+import PySimpleGUI as sg
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def openFile():
+    Tk().withdraw()
+    filename = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
+    return filename
 
+def calculate_sfi(etof, ntof, npl, epl, ets, nts, eit, nit):
+    """
+    Calculates the Sciatic Function Index (SFI) based on input parameters.
+    :param etof: Experimental TOF (orthogonal distance)
+    :param ntof: Normal TOF (orthogonal distance)
+    :param npl: Normal print length
+    :param epl: Experimental print length
+    :param ets: Experimental total spread
+    :param nts: Normal total spread
+    :param eit: Experimental intermediate toes
+    :param nit: Normal intermediate toes
+    """
+    sfi = ((etof - ntof) / ntof) + ((npl - epl) / epl) + ((ets - nts) / nts) + ((eit - nit) / nit) * 220
+    return sfi
 
-# Press the green button in the gutter to run the script.
+def showVideo():
+
+    #  choose and read video
+    chosenVideoPath = openFile()  # select video to show
+    video = cv2.VideoCapture(chosenVideoPath)
+
+    layout = [[sg.Graph((video.get(cv2.CAP_PROP_FRAME_WIDTH), video.get(cv2.CAP_PROP_FRAME_HEIGHT)), (0, video.get(cv2.CAP_PROP_FRAME_HEIGHT)), (video.get(cv2.CAP_PROP_FRAME_WIDTH), 0), key='-GRAPH-', enable_events=True, drag_submits=True)], ]
+    window = sg.Window('SFI Calculator', layout)
+    graph_elem = window['-GRAPH-']  # type: sg.Graph
+    a_id = None
+    #key = cv2.waitKey(0)
+    # show video
+    while True:
+        event, values = window.read(timeout=0)
+        if event in ('Exit', None):
+            break
+
+        ret, frame = video.read()
+
+        #if key == ord('p'):
+         #   cv2.waitKey(-1)
+        imgbytes = cv2.imencode('.ppm', frame)[1].tobytes()
+        if a_id:
+            graph_elem.delete_figure(a_id)  # delete previous image
+        a_id = graph_elem.draw_image(data=imgbytes, location=(0, 0))  # draw new image
+        graph_elem.send_figure_to_back(a_id)  # move image to the "bottom" of all other drawings
+
+        if event == '-GRAPH-':
+            graph_elem.draw_circle(values['-GRAPH-'], 5, fill_color='red', line_color='red')
+
+    window.close()
+
+    video.release()
+    cv2.destroyAllWindows()
+
+# main function
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    showVideo()
+    print("Test SFI run: " + str(calculate_sfi(10,5,15,12,8,7,6,5)))
